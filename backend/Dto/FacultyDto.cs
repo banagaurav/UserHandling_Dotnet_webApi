6878dbc288dc400215a345506a3375aac45676b7
Your Name <gauravbana840@gmail.com>
2025-01-17 17:19:17 +0545

    add Delete Faculty

---
+++ backend/Dto/FacultyDto.cs
@@ -0,0 +1,7 @@
+public class FacultyDto
+{
+    public int Id { get; set; }
+    public string Name { get; set; } = string.Empty;
+    public int UniversityId { get; set; }
+    public string UniversityName { get; set; } = string.Empty;
+}
